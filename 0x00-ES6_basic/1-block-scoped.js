2e48e08253581fca5d83ad73a722bc5694066f9d
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-07-25 19:06:38 +0300

    task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueorFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueorFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
