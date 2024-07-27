552470710905539d0126412b2a5a750e5607c0b6
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-07-27 10:47:56 +0300

    task10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const copyArray = [];
   for (const idx of array) {
-    const value = array[idx];
-    copyArray[idx] = appendString + value;
+    copyArray.push(appendString + idx);
   }
   return copyArray;
 }
