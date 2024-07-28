6955194115170b3dc30a2a07a76e7bee26a24605
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-07-28 13:55:51 +0300

    task12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+  const employees = { ...employeesList };
+  let counter = 0;
+  function getNumberOfDepartments(employees) {
+    counter = Object.keys(employees).length;
+    return counter;
+  }
+
+  return {
+    allEmployees: employees,
+    getNumberOfDepartments: getNumberOfDepartments
+  };
+}
