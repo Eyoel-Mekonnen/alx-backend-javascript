525f5067faed7f2fdc66e4f3043af4ada4b69216
root <root@DESKTOP-B19RQ5P>
2024-07-30 12:20:00 +0300

    task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
-  Promise.all([promise1, promise2])
+  return Promise.all([promise1, promise2])
     .then((values) => {
       const result1 = values[0];
       const result2 = values[1];
