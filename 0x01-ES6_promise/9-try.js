5882c08c69e79fa98f73c9bc2cd5a0838a7ebe90
root <root@DESKTOP-B19RQ5P>
2024-07-31 17:16:09 +0300

    task9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const array =[];
+
+  try {
+    array.push(mathFunction());
+  } catch (error) {
+    const err = error.message;
+    const message = 'Error: ' + err;
+    array.push(message);
+  }
+  array.push('Guardrail was processed');
+  return array;
+}
