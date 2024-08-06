cce0809881547992112cc48e3387ec23cd1e2aac
root <root@DESKTOP-B19RQ5P>
2024-08-06 15:38:02 +0300

    task5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,9 @@
 export default function (length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
   const buffer = new ArrayBuffer(length);
   const int8 = new Int8Array(buffer);
-  if (position >= length || position < 0) {
-    throw Error('Position outside range');
-  } else {
-    int8[position] = value;
-  }
+  int8[position] = value;
   return buffer;
 }
