46f8452cabf92321e64f3c9dd953cb1e3480bf5b
root <root@DESKTOP-B19RQ5P>
2024-08-06 16:53:51 +0300

    task5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,8 @@ export default function (length, position, value) {
   if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
-  const buffer = new DataView(new ArrayBuffer(length), 0, length);
-  buffer.setInt8(position, value);
-  return buffer;
+  const buffer = new ArrayBuffer(10);
+  const viewBuff = new DataView(buffer);
+  viewBuff.setInt8(position, value);
+  return viewBuff;
 }
