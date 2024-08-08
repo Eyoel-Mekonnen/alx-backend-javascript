becde5c2f019b3693e055f1f1c80d74a32c76be2
root <root@DESKTOP-B19RQ5P>
2024-08-08 17:02:09 +0300

    task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,15 +5,13 @@ export default function cleanSet(set, startString) {
   }
   for (const value of set) {
     if (typeof value === 'string' && value.startsWith(startString)) {
-        const sliced = value.slice(startString.length);
-	if (concat) {
-          concat += `-${sliced}`;
-        } else {
-          concat = sliced;
-        }
+      const sliced = value.slice(startString.length);
+      if (concat) {
+        concat += `-${sliced}`;
       } else {
-        continue;
+        concat = sliced;
       }
     }
-    return concat;
+  }
+  return concat;
 }
