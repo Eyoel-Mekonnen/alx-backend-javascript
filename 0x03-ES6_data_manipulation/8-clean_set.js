3d5cdab916eb9ea95c0f9052c7cdf983dceab69d
root <root@DESKTOP-B19RQ5P>
2024-08-08 16:58:52 +0300

    task8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,7 +6,6 @@ export default function cleanSet(set, startString) {
   for (const value of set) {
     if (typeof value === 'string' && value.startsWith(startString)) {
         const sliced = value.slice(startString.length);
-        console.log(sliced);
 	if (concat) {
           concat += `-${sliced}`;
         } else {
