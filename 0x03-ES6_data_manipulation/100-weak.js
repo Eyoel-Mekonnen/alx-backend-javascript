144418bfbbd9067d99296a102f81b2d745326e34
root <root@DESKTOP-B19RQ5P>
2024-08-09 00:18:33 +0300

    task100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,14 @@
-export let weakMap = new WeakMap();
-
+export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   let count;
   if (!(weakMap.has(endpoint))) {
     count = 1;
-    weakMap.set(endpoint, count);  
-    //console.log("Is it here");
+    weakMap.set(endpoint, count);
   } else {
     count = weakMap.get(endpoint);
     if (count >= 5) {
-      throw Error('Endpoint load is high');
+      throw new Error('Endpoint load is high');
     } else {
       count += 1;
       weakMap.set(endpoint, count);
