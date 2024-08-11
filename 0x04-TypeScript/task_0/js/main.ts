2c2f6508e7bca9e82a8f686c93ae43d1cd44e596
root <root@DESKTOP-B19RQ5P>
2024-08-11 11:10:11 +0300

    task0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -19,14 +19,42 @@ const student2: Student = {
   location: 'Addis Ababa',
 };
 
-function createTable(studentList: Student[]) {
-  let table = `<table>`;
-  table += `<tr><th>FirstName</th><th>LastName</th><th>Age</th><th>Location</th></tr>`;
-  studentList.forEach((item) => {
-    table += `<tr><td>${item.firstname}</td><td>${item.lastname}</td><td>${item.age}</td><td>${item.location}</td></tr>`;
-  });
-  table += `</table>`
+function createTable(studentLists: Student[]) {
+  const headerRows = ['FirstName', 'LastName', 'Age', 'Location'];
+  const table = document.createElement('table');
+  const tr1 = document.createElement('tr');
+  headerRows.forEach((headers) => {
+    const th = document.createElement('th');
+    th.textContent = headers;
+    tr1.appendChild(th);
+  })
+  table.appendChild(tr1);
+
+  studentLists.forEach((student) => {
+    const tr2 = document.createElement('tr');
+    const td1 = document.createElement('td');
+    td1.textContent = student.firstname;
+
+    tr2.appendChild(td1);
+
+    const td2 = document.createElement('td');
+    td2.textContent = student.lastname;
+    tr2.appendChild(td2);
+
+    const td3 = document.createElement('td');
+    td3.textContent = student.age.toString();
+
+    tr2.appendChild(td3);
+
+    const td4 = document.createElement('td');
+    td4.textContent = student.location;
+
+    tr2.appendChild(td4);
+
+    table.appendChild(tr2);
+  })
   return table;
 }
-const studentList = [student1, student2];
-createTable(studentList);
+const studentLists = [student1, student2];
+const table = createTable(studentLists);
+document.body.appendChild(table);
