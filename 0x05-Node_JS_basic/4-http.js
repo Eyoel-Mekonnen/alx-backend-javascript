59ab5f17ad11b4aae683076b28c0a180b485dc8e
root <root@DESKTOP-B19RQ5P>
2024-09-18 19:06:01 +0300

    fixed typo error

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const host = 'localhost';
 app.on('request', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello Holberton school!');
+  res.write('Hello Holberton School!');
   res.end();
 });
 app.listen(port, host, () => {
