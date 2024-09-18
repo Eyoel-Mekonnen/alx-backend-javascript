08d6f19e28491deaa03c2bdae7cdc9d519242b75
root <root@DESKTOP-B19RQ5P>
2024-09-18 15:53:15 +0300

    Asynchronus file reading

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const countStudents = (file) => new Promise((resolve, reject) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    const stringFormat = data.trim();
+    const dataRead = stringFormat.split('\n');
+    dataRead.shift();
+    const studentMajor = {};
+    let numberOfStudents = 0;
+    dataRead.forEach((line) => {
+      const fields = line.split(',');
+      const firstName = fields[0];
+      const major = fields[3];
+      if (!studentMajor[major]) {
+        studentMajor[major] = { name: [], count: 0 };
+      }
+      studentMajor[major].name.push(firstName);
+      studentMajor[major].count += 1;
+      numberOfStudents += 1;
+    });
+    Object.entries(studentMajor).forEach(([major, { name, count }]) => {
+      console.log(`Number of students in ${major}: ${count}. List: ${name.join(', ')}`);
+    });
+    resolve({ studentMajor, numberOfStudents });
+  });
+});
+module.exports = countStudents;
