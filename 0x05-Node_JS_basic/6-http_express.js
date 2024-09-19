c18ab885d867898bd6b476a9c77f497cc3efc2e8
root <root@DESKTOP-B19RQ5P>
2024-09-19 15:48:15 +0300

    simple express server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
 const app = express();
-const PORT = 1245;
+const port = 1245;
+const host = 'localhost';
 
 app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
-
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
+app.listen(port, host, () => {
+  console.log(`Server Listening on ${port}:${host}`);
 });
-
 module.exports = app;
